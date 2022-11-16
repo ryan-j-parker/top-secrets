6bfdb579ad97edf26b52f38c4a657967ec3f7768
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-11-16 14:57:10 -0800

    add test for users post route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,26 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
+const { request } = require('express');
 // const request = require('supertest');
-// const app = require('../lib/app');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  
+  it('POST /users should add a new user', async () => {
+    const testUser = {
+      email: 'user@email.com',
+      password: 'password12345',
+    };
+    const res = await request(app).post('/api/v1/users').send(testUser);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: 'user@email.com',
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
