5842b5a4f5e287ec80552aa348a2b6f4fe86bb8c
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-11-16 19:55:49 -0800

    add model, controller, and UserService file for users route

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,19 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
-DROP TABLE IF EXISTS secrets;
+-- DROP TABLE IF EXISTS secrets;
 
-CREATE TABLE secrets (
-    id BIGINT GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR NOT NULL,
-    description VARCHAR NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
+-- CREATE TABLE secrets (
+--     id BIGINT GENERATED ALWAYS AS IDENTITY,
+--     title VARCHAR NOT NULL,
+--     description VARCHAR NOT NULL,
+--     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+-- );
 
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
     email VARCHAR NOT NULL,
-    password VARCHAR NOT NULL
+    password_hash VARCHAR NOT NULL
 );
