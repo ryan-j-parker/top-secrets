1f6181b7867418109d7902e3d3d0cc594530f6f0
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-11-17 13:28:04 -0800

    add test for user sign in

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -18,6 +19,17 @@ describe('backend-express-template routes', () => {
       id: expect.any(String),
       email: 'user@email.com',
     });
+  });
+
+  it('POST /users/sessions should sign in a user', async () => {
+    const userData = {
+      email: 'user@email.com',
+      password: 'passwored12345'
+    };
+    const testUser = await UserService.create(userData);
+    const agent = request.agent(app);
+    const res = await (await agent.post('/api/v1/users/sessions')).send(userData);
+    expect(res.body).toEqual({ message: 'Signed in successfully', testUser });
   });
 
   afterAll(() => {
